4e0a4b0dfce3571dfe56bac48d2a195fcab8e504
ShakeelGadafi <shakeelgadhafi@gmail.com>
2025-07-05 23:50:18 +0530

    Page Object Model

--- cypress/e2e/loginPAge/loginpage.js
+++ cypress/e2e/loginPAge/loginpage.js
@@ -1,14 +1,19 @@
 export class Loginpage{
+
+username_textbox=':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input'
+passcode_textbox=':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input'
+Loginpage_btn='.oxd-button'
+    
 enterUsername(username){
-    cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type(username)
+    cy.get(this.username_textbox).type(username)
 }
 
 enterpasscode(passcode){
-    cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type(passcode)
+    cy.get(this.passcode_textbox).type(passcode)
 }
 
 clickbtn(){
-    cy.get('.oxd-button').click()
+    cy.get(this.Loginpage_btn).click()
 }
 
 }
